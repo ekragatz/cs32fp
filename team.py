68fce6bd5902727234de2ae0ab68bab1e58d004e
kavya-karra <kavyakarra@college.harvard.edu>
2025-05-05 17:51:50 -0400

    Update team.py

--- team.py
+++ team.py
@@ -1,17 +1,21 @@
 ### cs32_fp/team.py
 
 class Team:
+    # sets up a new team object with a name, optional list of players, and a starting score of 0
     def __init__(self, name, players=None):
         self.name = name
         self.players = players if players is not None else []
         self.score = 0
 
+    # adds a player's name to the team's player list
     def add_player(self, player_name):
         self.players.append(player_name)
 
+    # increases the team score by the number of points given
     def add_points(self, points):
         self.score += points
 
+    # subracts points from the team score but never lets it get below 0
     def subtract_points(self, points):
         self.score = max(0, self.score - points)
 
